8427399132a5675c13b278582e637db23980d1b7
sky_miao <mengmiao_sky@126.com>
2018-04-23 16:14:42 +0800

    使用element布局

---
+++ src/components/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <el-container>
+        <el-header>
+            <Header></Header>
+        </el-header>
+        <el-main>Main</el-main>
+        <el-footer>
+            <Footer></Footer>
+        </el-footer>
+    </el-container>
+</template>
+<script>
+import Header from "./header";
+import Footer from "./footer";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import index from '@/components/index'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
     }
