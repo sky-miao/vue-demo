7f53857471e5e2947466efd85cd3d8ac4d98c5e8
sky_miao <mengmiao_sky@126.com>
2018-04-26 13:59:14 +0800

    handleClose

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -7,16 +7,16 @@ const vueLoaderConfig = require('./vue-loader.conf')
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
-
+// eslint
 const createLintingRule = () => ({
-  test: /\.(js|vue)$/,
-  loader: 'eslint-loader',
-  enforce: 'pre',
-  include: [resolve('src'), resolve('test')],
-  options: {
-    formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
-  }
+  // test: /\.(js|vue)$/,
+  // loader: 'eslint-loader',
+  // enforce: 'pre',
+  // include: [resolve('src'), resolve('test')],
+  // options: {
+  //   formatter: require('eslint-friendly-formatter'),
+  //   emitWarning: !config.dev.showEslintErrorsInOverlay
+  // }
 })
 
 module.exports = {
--- src/components/dialog.vue
+++ src/components/dialog.vue
@@ -1,33 +1,32 @@
 <!--子组件-->
 <template>
   <!-- <el-dialog :visible.sync="currentShow"> -->
-  <el-dialog :visible.sync="show">
-      afsdsfadfasdfasd
+  <el-dialog :visible.sync="show"
+    :before-close="handleClose"
+    >
+    sdfggggggggggggg
   </el-dialog>
 </template>
 
 <script>
 import { mapState } from "vuex";
 export default {
-  // props: ["show"],
-  //   computed: {
-  //     currentShow: {
-  //       get() {
-  //         return this.show;
-  //       },
-  //       set(val) {
-  //         this.$emit("update:show", val);
-  //       }
-  //     }
-  //   }
+  methods: {
+    handleClose() {
+      this.$store.dispatch("switch_dialog");
+    },
+    // ...mapMutations({
+    //   switch_dialog: "handleClose(false)" // 映射 this.increment() 为 this.$store.commit('increment')
+    // })
+  },
   computed: {
     // 这里的三点叫做 : 扩展运算符
-    // ...mapState({
-    //   show: state => state.dialog.show
-    // })
-    show() {
-      return this.$store.state.dialog.show;
-    }
+    ...mapState({
+      show: state => state.dialog.show
+    })
+    // show() {
+    //   return this.$store.state.dialog.show;
+    // }
   }
 };
 </script>
